4dfeba0198685328fb7662c7fd7a1308c8e74557
June <81022107+vrijune@users.noreply.github.com>
2022-01-21 13:55:27 +1300

    java

---
+++ src/June/industry/controlflow/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,72 @@
+package ictgradschool.industry.controlflow.rockpaperscissors;
+
+import ictgradschool.Keyboard;
+
+import java.awt.*;
+import java.awt.print.Printable;
+
+/**
+ A game of Rock, Paper Scissors
+ */
+public class RockPaperScissors {
+
+    public static final int ROCK = 1;
+        public static final int Paper = 2;
+        public static final int Scissors = 3;
+        public static final int Quit = 4;
+        // TODO Make similar constants for PAPER and SCISSORS, to improve readability of your code.
+
+        public void start() {
+
+            System.out.println("Hi, What is your name?");
+            String playername = Keyboard.readInput();
+            System.out.println("1.Rock 2. Scissors 3.Paper 4.Quit");
+            System.out.println("Enter choice");
+            int choice = Integer.parseInt(Keyboard.readInput());
+            displayPlayerChoice(playername, choice);
+            int comchoice =（int)(Math.random()*4);
+            displayPlayerChoice("computer", comchoice);
+
+
+        // TODO Write your code here which calls your other methods in order to play the game. Implement this
+        // as detailed in the exercise sheet.
+    }
+
+
+    public void displayPlayerChoice(String name, int choice){
+
+
+
+
+
+        // TODO This method should print out a message stating that someone chose a particular thing (rock, paper or scissors)
+    }
+
+
+    public boolean userWins(int playerChoice, int computerChoice) {
+
+        // TODO Determine who wins and return true if the player won, false otherwise.
+        return false;
+    }
+
+    public String getResultString(int playerChoice, int computerChoice) {
+
+        final String PAPER_WINS = "paper covers rock";
+        final String ROCK_WINS = "rock smashes scissors";
+        final String SCISSORS_WINS = "scissors cut paper";
+        final String TIE = " you chose the same as the computer";
+
+        // TODO Return one of the above messages depending on what playerChoice and computerChoice are.
+        return null;
+    }
+
+    /**
+     * Program entry point. Do not edit.
+     */
+    public static void main(String[] args) {
+
+        RockPaperScissors ex = new RockPaperScissors();
+        ex.start();
+
+    }
+}
